b209830102e6cb22e2dd7f2529899454aadcb23a
RAB_5 <priyanshu00268.gmail.com>
2025-03-01 19:58:15 +0530

    testing 10

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hi there");
+    res.send("Hi there2");
 })
 
 app.post("/signup", async (req, res) => {
